86b4dd989bf1c834b17f2eeba969da9570576631
Colin Bernard <colinjbernard@hotmail.com>
2016-01-23 18:32:24 -0800

    Changed board to grid of buttons. Not resizable anymore.

--- src/chessgame/gui/GUI.java
+++ src/chessgame/gui/GUI.java
@@ -4,9 +4,6 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class GUI extends Application {
@@ -18,22 +15,22 @@ public class GUI extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		primaryStage.setTitle("Chess Game");
-		Button b = new Button();
-		b.setText("Play chess!");
 		
+		// init board
 		for(int row = 0; row < 8; row++) {
 			for(int col = 0; col < 8; col++) {
-				Rectangle square = new Rectangle();
-				Color color;
+				Button square = new Button();
+				String color;
 				if((row + col) % 2 == 0) {
-					color = Color.WHITE;
+					color = "#ffffff"; // white
 				} else {
-					color = Color.BLACK;
+					color = "#000000"; // black
 				}
-				square.setFill(color);
+				square.setStyle("-fx-base: " + color); // set button color using CSS
+				// set button sizes
+				square.setMinHeight(50);
+				square.setMinWidth(50);
 				root.add(square, col, row);
-				square.widthProperty().bind(root.widthProperty().divide(8));
-				square.heightProperty().bind(root.heightProperty().divide(8));
 			}
 		}
 		primaryStage.setScene(new Scene(root, FRAME_WIDTH, FRAME_HEIGHT));
