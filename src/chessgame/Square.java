efa42a95dacbd1b6af956e0293b2f7ad7165ce2c
Mackenzie Salloum <mackenziesalloum@gmail.com>
2016-01-24 21:45:22 -0800

    added basic checking methods.

--- src/chessgame/Square.java
+++ src/chessgame/Square.java
@@ -8,16 +8,19 @@ import chessgame.pieces.Piece;
 public class Square extends Button{
 	private Color color; // black or white square
 	private Piece piece; // should be null if square is unoccupied
-	
+
+	public Square(Color color){
+		this.color = color;
+	}
 	public Square(Color color, Piece piece) {
 		this.color = color;
 		this.piece = piece;
 	}
-	private boolean isOccupied = false;
-	private void setOccupied(boolean s){
-		isOccupied = s;
+	
+	private void setPiece(Piece p){
+		this.piece = p;
 	}
 	private boolean checkOccupied(){
-		
+		return piece!=null;
 	}
 }
