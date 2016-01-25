532aaabad8381e8d51e3ed2f11874e7d7a974789
Mackenzie Salloum <mackenziesalloum@gmail.com>
2016-01-24 21:29:06 -0800

    added branch to work on square properties.

--- src/chessgame/Square.java
+++ src/chessgame/Square.java
@@ -2,14 +2,22 @@ package chessgame;
 
 import java.awt.Color;
 
+import javafx.scene.control.Button;
 import chessgame.pieces.Piece;
 
-public class Square {
+public class Square extends Button{
 	private Color color; // black or white square
 	private Piece piece; // should be null if square is unoccupied
 	
 	public Square(Color color, Piece piece) {
 		this.color = color;
 		this.piece = piece;
+	}
+	private boolean isOccupied = false;
+	private void setOccupied(boolean s){
+		isOccupied = s;
+	}
+	private boolean checkOccupied(){
+		
 	}
 }
