96104229a1c399b3785aaf61f0a164a20aa87a88
Colin Bernard <colinjbernard@hotmail.com>
2016-02-24 21:31:54 -0800

    Added tool bar and New/Quit buttons.

--- src/chessgame/gui/GUI.java
+++ src/chessgame/gui/GUI.java
@@ -3,28 +3,42 @@ package chessgame.gui;
 import chessgame.Square;
 import chessgame.pieces.Piece;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ToolBar;
 import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 
 public class GUI extends Application {
 	private static final int FRAME_WIDTH = 560;
-	private static final int FRAME_HEIGHT = 560;
+	private static final int FRAME_HEIGHT = 595; // height of toolbar is 35px
 	private static final String TITLE = "Chess Game";
 	
-	private GridPane root = new GridPane();
+	private Stage primaryStage;
+	private GridPane gridPane = new GridPane();
+	private ToolBar toolbar1;
 	public static Square[][] board;
 	
 	@Override
 	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
 		primaryStage.setTitle(TITLE);
 		primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/res/pawnb.png"))); // set application icon
 		
 		initBoard();
+		initToolBar();
 		
-		primaryStage.setScene(new Scene(root, FRAME_WIDTH, FRAME_HEIGHT));
+		BorderPane root = new BorderPane();
+		root.setTop(toolbar1);
+		root.setCenter(gridPane);
+		Scene scene = new Scene(root, FRAME_WIDTH, FRAME_HEIGHT);
+		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
 	
@@ -53,10 +67,34 @@ public class GUI extends Application {
 				
 				// add event handler to button
 				square.setOnAction(new Handler());
-				root.add(square, col, row);
+				gridPane.add(square, col, row);
 				board[row][col] = square; // store this square in array so it can be accessed without clicking on it
 			}
 		}
+	}
+	
+	private void initToolBar() {
+		Button btnNew = new Button("New");
+		Button btnQuit = new Button("Quit");
+		toolbar1 = new ToolBar();
+		toolbar1.getItems().addAll(btnNew, btnQuit);
+		toolbar1.setPrefWidth(FRAME_WIDTH);
+		
+		// button listeners
+		btnNew.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				primaryStage.close();
+				start(new Stage());
+				
+			}
+		});
+		btnQuit.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				primaryStage.close();
+			}
+		});
 	}
 
 	public static void main(String[] args) {
