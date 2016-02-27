565d55c9e84d5d85d400cf134c7bae981e3db6cb
Colin Bernard <colinjbernard@hotmail.com>
2016-02-26 21:47:22 -0800

    Deleted Rules.java, not needed.

--- src/chessgame/gui/GUI.java
+++ src/chessgame/gui/GUI.java
@@ -4,7 +4,6 @@ import chessgame.Square;
 import chessgame.pieces.Piece;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
-import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
